489efb4524a32966746853ad40d8b2edf4c94d10
Shukhrat Gadoev <shukhratgadoev@77projects.com>
2023-04-15 16:07:36 +0500

    feat: finish whatisBlock

--- src/components/WhatIsBlock/WhatIsBlock.tsx
+++ src/components/WhatIsBlock/WhatIsBlock.tsx
@@ -9,39 +9,48 @@ import {
   WhatIsBlockList,
   WhatIsBlockImgSection,
   WhatIsBlockImg,
+  WhatIsBlockButtonBtnWrraper,
+  WhatIsBlockButtonBtn,
+  WhatIsBlockMain,
 } from "./styled";
 
 const WhatIsBlock: React.FC = () => {
   return (
-    <WhatIsBlockWrapper>
-      <WhatIsBlockCover>
-        <WhatIsBlockTitle>What is Lorem Ipsum</WhatIsBlockTitle>
-        <WhatIsBlockParagraf>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries
-        </WhatIsBlockParagraf>
-        <WhatIsBlockUl>
-          <WhatIsBlockList>
-            <AiOutlineCheckCircle />
-            Lorem Ipsum is simply dummy text of the printing
-          </WhatIsBlockList>
-          <WhatIsBlockList>
-            <AiOutlineCheckCircle />
-            Lorem Ipsum is simply dummy text of the printing
-          </WhatIsBlockList>
-          <WhatIsBlockList>
-            <AiOutlineCheckCircle />
-            Lorem Ipsum is simply dummy text of the printing
-          </WhatIsBlockList>
-        </WhatIsBlockUl>
-      </WhatIsBlockCover>
-      <WhatIsBlockImgSection>
-        <WhatIsBlockImg />
-      </WhatIsBlockImgSection>
-    </WhatIsBlockWrapper>
+    <WhatIsBlockMain>
+      <WhatIsBlockWrapper>
+        <WhatIsBlockCover>
+          <WhatIsBlockTitle>What is Lorem Ipsum</WhatIsBlockTitle>
+          <WhatIsBlockParagraf>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries
+          </WhatIsBlockParagraf>
+          <WhatIsBlockUl>
+            <WhatIsBlockList>
+              <AiOutlineCheckCircle />
+              &nbsp; Lorem Ipsum is simply dummy text of the printing
+            </WhatIsBlockList>
+            <WhatIsBlockList>
+              <AiOutlineCheckCircle />
+              &nbsp; Lorem Ipsum is simply dummy text of the printing
+            </WhatIsBlockList>
+            <WhatIsBlockList>
+              <AiOutlineCheckCircle />
+              &nbsp; Lorem Ipsum is simply dummy text of the printing
+            </WhatIsBlockList>
+          </WhatIsBlockUl>
+        </WhatIsBlockCover>
+        <WhatIsBlockImgSection>
+          <WhatIsBlockImg />
+        </WhatIsBlockImgSection>
+      </WhatIsBlockWrapper>
+
+      <WhatIsBlockButtonBtnWrraper>
+        <WhatIsBlockButtonBtn>Help Me</WhatIsBlockButtonBtn>
+      </WhatIsBlockButtonBtnWrraper>
+    </WhatIsBlockMain>
   );
 };
 
--- src/components/WhatIsBlock/styled.tsx
+++ src/components/WhatIsBlock/styled.tsx
@@ -1,4 +1,9 @@
 import styled from "styled-components";
+import Man from "../../data/man.png";
+
+export const WhatIsBlockMain = styled.div`
+  background-color: #f2f2f2;
+`;
 
 export const WhatIsBlockWrapper = styled.div`
   background-color: #f2f2f2;
@@ -6,10 +11,29 @@ export const WhatIsBlockWrapper = styled.div`
   justify-content: space-between;
   align-items: center;
   padding-top: 23px;
+  @media (max-width: 992px) {
+    flex-direction: column;
+    justify-content: center;
+  }
 `;
 export const WhatIsBlockCover = styled.div`
   width: 100%;
   max-width: 518px;
+  padding-right: 77px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: start;
+  width: 100%;
+  margin-left: auto;
+  @media (max-width: 1280px) {
+    max-width: 410px;
+    padding-right: 20px;
+  }
+  @media (max-width: 992px) {
+    margin: 0;
+    text-align: center;
+  }
 `;
 
 export const WhatIsBlockTitle = styled.h2`
@@ -20,6 +44,13 @@ export const WhatIsBlockTitle = styled.h2`
   line-height: 72px;
   margin-top: 0;
   margin-bottom: 20px;
+  @media (max-width: 1280px) {
+    font-size: 28px;
+    line-height: 28px;
+  }
+  @media (max-width: 992px) {
+    width: inherit;
+  }
 `;
 
 export const WhatIsBlockParagraf = styled.p`
@@ -30,10 +61,15 @@ export const WhatIsBlockParagraf = styled.p`
   line-height: 22px;
   margin-top: 0;
   margin-bottom: 30px;
+  @media (max-width: 1280px) {
+    font-size: 12px;
+    margin-bottom: 5px;
+  }
 `;
 
 export const WhatIsBlockUl = styled.ul`
   list-style: none;
+  padding: 0;
 `;
 
 export const WhatIsBlockList = styled.li`
@@ -45,6 +81,7 @@ export const WhatIsBlockList = styled.li`
   display: flex;
   align-items: center;
   height: 18px;
+  margin-bottom: 20px;
 `;
 
 export const WhatIsBlockImgSection = styled.div`
@@ -54,6 +91,70 @@ export const WhatIsBlockImgSection = styled.div`
   background-color: #50b2ea;
   border-top-left-radius: 40px;
   border-bottom-left-radius: 40px;
+  position: relative;
+  @media (max-width: 1280px) {
+    height: 380px;
+  }
+  @media (max-width: 992px) {
+    max-width: inherit;
+    border-radius: 0;
+  }
 `;
 
-export const WhatIsBlockImg = styled.div``;
+export const WhatIsBlockImg = styled.div`
+  position: absolute;
+  top: 77px;
+  left: 13px;
+  background-image: url(${Man});
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+  width: 100%;
+  height: 100%;
+  max-width: 559px;
+  max-height: 509px;
+  @media (max-width: 1280px) {
+    background-size: contain;
+    max-width: 390px;
+    max-height: 350px;
+  }
+  @media (max-width: 992px) {
+    top: 20%;
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    left: 0;
+    right: 0;
+    margin: auto;
+    border-radius: 40px;
+    background-size: cover;
+  }
+`;
+
+export const WhatIsBlockButtonBtnWrraper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const WhatIsBlockButtonBtn = styled.button`
+  background: rgb(80, 178, 234);
+  box-shadow: rgba(129, 209, 255, 0.2) 0px 20px 40px;
+  border-radius: 10px;
+  width: 160px;
+  height: 54px;
+  color: rgb(255, 255, 255);
+  border: 1px solid #fff;
+  font-family: Poppins;
+  font-style: normal;
+  font-weight: 700;
+  font-size: 18px;
+  line-height: 22px;
+  cursor: pointer;
+
+  margin-top: 127px;
+  margin-bottom: 50px;
+  @media (max-width: 1280px) {
+    margin-top: 60px;
+  }
+`;
